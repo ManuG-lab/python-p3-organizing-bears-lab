65d2da897ce1e941c444b2eae090e472ede45501
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-21 11:32:38 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate',3, 'M', 'brown', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',37, 'M', 'black', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tbitha',17, 'F', 'brown', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',12, 'F', 'brown', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',20, 'M', 'purple', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',2, 'F', 'brown', 'calm', 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES( null,40, 'M', 'brown', 'calm', 1)
